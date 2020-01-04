b5c73fb69cc671203e28af2bdefeec4221c385b6
Chris Hundhausen <chundhau@gmail.com>
2020-01-03 20:39:16 -0800

    roundForm updated to include two elts

---
+++ scripts/roundForm-complete.js
@@ -0,0 +1,39 @@
+class RoundForm extends React.Component {
+    constructor(props) {
+      super(props);
+      //Create date object for today, taking time zone into consideration
+      let today = new Date(Date.now()-(new Date()).getTimezoneOffset()*60000);
+      //store date as ISO string
+      this.state = {date:  today.toISOString().substr(0,10)}; 
+    }
+  
+    handleChange = (event) => {
+      this.setState({date: event.target.value});
+    }
+  
+    handleSubmit = (event) => {
+      alert('The form was submitted!');
+      event.preventDefault();
+    }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <center>
+            <label>
+              Date:
+              <input id="roundDate" className="form-control form-center" 
+                type="date" value={this.state.date} onChange={this.handleChange} />
+            </label>
+          <p></p>
+          <input type="submit" className="btn btn-primary" value="Submit" />
+          </center>
+        </form>
+      );
+    }
+}
+
+  ReactDOM.render(
+    <RoundForm />,
+    document.getElementById('root')
+  );
---
+++ scripts/roundForm.js
@@ -0,0 +1,48 @@
+class RoundForm extends React.Component {
+    constructor(props) {
+      super(props);
+      //Create date object for today, taking time zone into consideration
+      let today = new Date(Date.now()-(new Date()).getTimezoneOffset()*60000);
+      //store date as ISO string
+      this.state = {date:  today.toISOString().substr(0,10), 
+                    course: ''}; 
+    }
+  
+    handleChange = (event) => {
+      const name = event.target.name;
+      this.setState({[name]: event.target.value});
+    }
+  
+    handleSubmit = (event) => {
+      alert('The form was submitted!');
+      event.preventDefault();
+    }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <center>
+            <label>
+              Date:
+              <input name="date" className="form-control form-center" 
+                type="date" value={this.state.date} onChange={this.handleChange} />
+            </label>
+            <p></p>
+            <label>
+              Course:
+              <input name="course" className="form-control form-center" type="text"
+                value={this.state.course} onChange={this.handleChange}
+                placeholder="Course played" size="50" maxLength="50" />
+            </label>
+          <p></p>
+          <input type="submit" className="btn btn-primary" value="Submit" />
+          </center>
+        </form>
+      );
+    }
+}
+
+  ReactDOM.render(
+    <RoundForm />,
+    document.getElementById('root')
+  );
